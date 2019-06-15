4ae36bd82d0ef35470919408b2081babf4520c1e
Eric Rivera <ericrivera228@gmail.com>
2019-06-15 01:59:02 -0600

    Added hamburger-button component.

--- src/app/ui-module/ui.module.ts
+++ src/app/ui-module/ui.module.ts
@@ -1,22 +1,28 @@
 // Angular Imports
 import { NgModule } from '@angular/core';
-import { DataTableComponent } from './data-table/data-table.component';
 
 // Shared module imports
 import { MaterialDesignModule } from '@shared';
+
+// UI Module imports
+import { DataTableComponent } from './data-table/data-table.component';
+import { HamburgerButtonComponent } from './hamburger-button/hamburger-button.component';
 
 @NgModule({
 	imports: [
 		MaterialDesignModule
 	],
 	declarations: [
-		DataTableComponent
+		DataTableComponent,
+		HamburgerButtonComponent
 	],
 	exports: [
-		DataTableComponent
+		DataTableComponent,
+		HamburgerButtonComponent
 	]
 })
 export class UiModule { }
 
 // Export components that belong to this module
 export { DataTableComponent } from './data-table/data-table.component';
+export { HamburgerButtonComponent } from './hamburger-button/hamburger-button.component';
