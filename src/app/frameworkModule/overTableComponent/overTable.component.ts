ba919abe214c8bcc37212a2334641ebc35534714
Eric Rivera <ericrivera228@gmail.com>
2019-06-14 22:45:01 -0600

    Refactored some component and module names for proper convention and consistency.

--- src/app/frameworkModule/overTableComponent/overTable.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'ot-root',
-  templateUrl: './overTable.component.html'
-})
-export class OverTableComponent {
-  title = 'overTable';
-}
