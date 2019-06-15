c24daa96cbb296d31aecec5e692e90086adb2e2e
Eric Rivera <ericrivera228@gmail.com>
2019-06-14 22:45:43 -0600

    Refactored some component and module names for proper convention and consistency (forgot to commit these files in previous check-in).

---
+++ src/app/framework-module/framework.module.ts
@@ -0,0 +1,26 @@
+// Angular imports
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+// Page module imports
+import { BaseRoutesModule } from '@base-routes';
+
+// Local imports
+import { OverTableRoutingModule } from './overtable-routing.module';
+import { OvertableComponent } from './overtable/overtable.component';
+
+@NgModule({
+  declarations: [
+    OvertableComponent
+  ],
+  imports: [
+    BrowserModule,
+    BaseRoutesModule,
+    OverTableRoutingModule
+  ],
+  providers: [],
+  bootstrap: [
+    OvertableComponent
+  ]
+})
+export class FrameworkModule { }
