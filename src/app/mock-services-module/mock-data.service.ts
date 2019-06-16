eb8986bd80cb0646a65abaff6dd568158c3ae356
Eric Rivera <ericrivera228@gmail.com>
2019-06-15 21:50:02 -0600

    Added big mock data.

--- src/app/mock-services-module/mock-data.service.ts
+++ src/app/mock-services-module/mock-data.service.ts
@@ -24,4 +24,23 @@ export class MockDataService {
 		return of(new HttpResult<any[][]>(MockData.basicTableData, null));
 	}
 
+	/**
+	 * Returns a big array data
+	 * @return {Observable} [description]
+	 */
+	get bigBasicTableData(): Observable<HttpResult<any[][]>> {
+
+		// Base data set to work with
+		let baseData = MockData.basicTableData;
+
+		// Splice out the header, that only needs to be added once
+		let multipliedData = baseData.splice(0, 1);
+
+		// Loop a couple of times to create a big data array to return
+		for (let counter = 0; counter < 5; counter++) {
+			multipliedData = [...multipliedData, ...baseData];
+		}
+
+		return of(new HttpResult<any[][]>(multipliedData, null));
+	}
 }
