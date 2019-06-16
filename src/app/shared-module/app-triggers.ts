3ddcbe7205a91447180ed51199831d95286d5169
Eric Rivera <ericrivera228@gmail.com>
2019-06-16 03:12:56 -0600

    Fixed animation bugs.

--- src/app/shared-module/app-triggers.ts
+++ src/app/shared-module/app-triggers.ts
@@ -1,24 +1,34 @@
 // Angular imports
-import { query, stagger, style, transition, trigger} from '@angular/animations';
+import { animate, group, query, stagger, style, transition, trigger} from '@angular/animations';
 
 // Share module imports
 import { AppAnimations } from './app-animations';
 
 export class AppTriggers {
 
+	static readonly timeUnit = 'ms';
+	static readonly staggerTime: number = 25;
+
 	static readonly tableFadeInOut = trigger('tableFadeInOut', [
 		transition(':enter', [
-			query('tr', [
+			query('.fade-row, .detail-tr', [
 				style({ opacity: 0 }),
 			]),
-			query('tr', stagger('25ms', AppAnimations.fadeIn))
+			group([
+				query('.fade-row', stagger(`${AppTriggers.staggerTime}${AppTriggers.timeUnit}`, AppAnimations.fadeIn)),
+				query('.detail-tr', [stagger(`${AppTriggers.staggerTime}${AppTriggers.timeUnit}`, AppAnimations.fadeIn)], { optional: true })
+			])
+
 		]),
 		transition(':leave', [
 			style({ position: 'absolute', top: 0 }),
-			query('tr', [
+			query('.fade-row', [
 				style({ opacity: 1 }),
 			]),
-			query('tr', stagger('25ms', AppAnimations.fadeOut))
+			group([
+				query('.fade-row', stagger(`${AppTriggers.staggerTime}${AppTriggers.timeUnit}`, AppAnimations.fadeOut)),
+				query('.detail-tr', [stagger(`${AppTriggers.staggerTime}${AppTriggers.timeUnit}`, AppAnimations.fadeOut)], { optional: true })
+			])
 		])
 	]);
 
